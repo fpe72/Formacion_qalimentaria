cfdaa68698053eb4ed492eff8f782b491d0b265e
fpe72 <fernando.palacios@coitim.es>
2025-04-05 15:12:34 +0000

    050420245_1712

--- backend/models/Diploma.js
+++ backend/models/Diploma.js
@@ -12,12 +12,28 @@ const diplomaSchema = new mongoose.Schema({
     ref: "FinalExam",
     required: true,
   },
+  name: {
+    type: String,
+    required: true,
+  },
+  dni: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
   serial: {
     type: String,
     required: true,
-    unique: true, // número de registro único
+    unique: true,
   },
-  pdfPath: {
+  verificationURL: {
     type: String,
     required: true,
   },
